f59f6fade0191520294f237731b7e7332c15e444
Scarygami <scarygami@gmail.com>
2014-01-03 23:42:31 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-journey-to-the-moon
-===================
+# Journey to the Moon
+
+
+### License
+
+```
+Copyright (c) 2014 Gerwin Sturm, FoldedSoft e.U.
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not
+use this file except in compliance with the License. You may obtain a copy of
+the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+License for the specific language governing permissions and limitations under
+the License
+```
+
+### Image licenses
+
+```
+Solar System Icons
+http://www.iconspedia.com/pack/solar-system-1642/
+
+By: Dan Wiersema
+Website: www.danwiersema.com
+
+License:
+Free for non-commercial use. These icons are only for personal use only. You may not redistribute or modify these icons. Distributing these icons commercially or using them with intent for monetary gain is prohibited.
+```
+
+```
+Rocket Icon
+http://www.iconspedia.com/icon/rocket--281.html
+
+By: Aha-Soft
+Website: www.777icons.com/libs/transport-vista-icons.htm
+
+License:
+Free for non-commercial use.
+```
---
+++ static/journey.js
@@ -0,0 +1,153 @@
+(function (global) {
+  "use strict";
+  var
+    doc = global.document, con = global.console, dropbox, locations, current, distance, dateDiv, travelledDiv, leftDiv, rocket,
+    total = 384400, km2miles = 0.621371, R = 6371, deg2rad = Math.PI / 180;
+
+  dropbox = doc.getElementById("dropbox");
+  dateDiv = doc.getElementById("date");
+  travelledDiv = doc.getElementById("travelled");
+  leftDiv = doc.getElementById("left");
+  rocket = doc.getElementById("rocket");
+
+  Date.prototype.niceDate = function () {
+    var y, m, d;
+    y = this.getFullYear().toString();
+    m = (this.getMonth() + 1).toString();
+    d  = this.getDate().toString();
+    return y + "-" + (m[1] ? m : "0" + m[0]) + "-" + (d[1] ? d : "0" + d[0]);
+  };
+
+  Number.prototype.formatWithCommas = function () {
+    return Math.round(this).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  };
+
+  // Haversine formula
+  function distanceFromLatLng(lat1, lon1, lat2, lon2) {
+    var dLat, dLon, a, c;
+
+    dLat = (lat2 - lat1) * deg2rad / 2;
+    dLon = (lon2 - lon1) * deg2rad / 2;
+    lat1 = lat1 * deg2rad;
+    lat2 = lat2 * deg2rad;
+
+    a = Math.sin(dLat) * Math.sin(dLat) + Math.sin(dLon) * Math.sin(dLon) * Math.cos(lat1) * Math.cos(lat2);
+    c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    return R * c;
+  }
+
+  function travel() {
+    var location = locations[current], location1, location2, lat1, lon1, lat2, lon2, time1, time2, perc, left;
+
+    time1 = new Date(parseInt(location.timestampMs, 10));
+    time2 = new Date(parseInt(location.timestampMs, 10));
+
+    // putting all locations of one day together
+    while (current > 0 && time2.getFullYear() == time1.getFullYear() && time2.getMonth() == time1.getMonth() && time2.getDate() == time1.getDate()) {
+      location1 = locations[current];
+      location2 = locations[current - 1];
+
+      lat1 = location1.latitudeE7 / 10000000;
+      lon1 = location1.longitudeE7 / 10000000;
+      lat2 = location2.latitudeE7 / 10000000;
+      lon2 = location1.longitudeE7 / 10000000;
+
+      distance += distanceFromLatLng(lat1, lon1, lat2, lon2);
+      time2 = new Date(parseInt(location2.timestampMs, 10));
+      current -= 1;
+    }
+
+    perc = distance / total * 100;
+    if (perc > 100) { perc = 100; }
+
+    dateDiv.innerHTML = time1.niceDate();
+
+    travelledDiv.style.width = perc + "%";
+    leftDiv.style.width = Math.min(100 - perc, 100) + "%";
+    leftDiv.style.left = perc + "%";
+    rocket.style.left = perc + "%";
+    travelledDiv.innerHTML = distance.formatWithCommas() + " km<br>" + (distance * km2miles).formatWithCommas() + " miles";
+
+    left = total - distance;
+    if (left < 0) { left = 0; }
+    leftDiv.innerHTML = left.formatWithCommas() + " km<br>" + (left * km2miles).formatWithCommas() + " miles";
+
+    if (!travelledDiv.style.paddingRight) {
+      if (travelledDiv.offsetWidth > 25) {
+        travelledDiv.style.paddingRight = "25px";
+      }
+    }
+
+    if (current > 0) {
+      global.requestAnimationFrame(travel);
+    }
+  }
+
+  function start() {
+    dropbox.style.display = "none";
+    doc.getElementById("instructions").style.display = "none";
+    global.requestAnimationFrame(travel);
+  }
+
+  function handleFile(file) {
+    var reader, data;
+    reader = new global.FileReader();
+
+    reader.onload = function (eventObj) {
+      data = {};
+      try {
+        data = JSON.parse(eventObj.target.result);
+      } catch (e) {
+        dropbox.innerHTML = "Start aborted!<br>Invalid file format.";
+        return;
+      }
+      if (data && data.locations && data.locations.length > 0) {
+        // check first location for valid format
+        if (data.locations[0].timestampMs && data.locations[0].latitudeE7 && data.locations[0].longitudeE7) {
+          locations = data.locations;
+          dropbox.innerHTML = "Ready for Take-off!";
+          current = locations.length - 1;
+          distance = 0;
+          global.setTimeout(start, 1000);
+        } else {
+          dropbox.innerHTML = "Start aborted!<br>No valid location found in file.";
+        }
+      } else {
+        dropbox.innerHTML = "Start aborted!<br>No locations found in file.";
+      }
+    };
+
+    reader.readAsText(file);
+  }
+
+  function handleDragOver(eventObj) {
+    eventObj.stopPropagation();
+    eventObj.preventDefault();
+    eventObj.dataTransfer.dropEffect = "copy";
+  }
+
+  function handleFileSelect(eventObj) {
+    var files;
+    eventObj.stopPropagation();
+    eventObj.preventDefault();
+
+    files = eventObj.dataTransfer.files;
+    if (files && files.length > 0) {
+      dropbox.innerHTML = "Preparing start!";
+      handleFile(files[0]);
+    }
+
+    dropbox.className = "";
+  }
+
+  dropbox.addEventListener("dragover", handleDragOver, false);
+  dropbox.addEventListener("drop", handleFileSelect, false);
+  dropbox.addEventListener("dragenter", function () {
+    dropbox.className = "dragging";
+  }, false);
+  dropbox.addEventListener("dragleave", function () {
+    dropbox.className = "";
+  }, false);
+
+}(this));
