ae5ea3fbc285a8f9ea0521da93057adb8b5c41a6
Scarygami <scarygami@gmail.com>
2014-01-08 15:39:38 +0100

    Fix cities.json for Stefan

--- static/scripts/journey.js
+++ static/scripts/journey.js
@@ -103,7 +103,7 @@
       }
     };
 
-    xhr.open("GET", "cities.json?v=2", true);
+    xhr.open("GET", "cities.json?v=3", true);
     xhr.send();
 
     travelWorker = new global.Worker('scripts/travelWorker.js');
